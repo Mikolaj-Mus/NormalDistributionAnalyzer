e6017a1d4fad01e287a79eccaaef15d3881fab1d
Mikolaj-Mus <mus_mikolaj@wp.pl>
2023-11-11 20:39:50 +0100

    Divide methods into smaller

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ class NormalDistributionAnalyzer {
   #numberOfArrays;
   #arrPartSingle;
   #arrPartMultiple;
+  #avgSingle;
+  #avgMultiple;
+  #arrays;
 
   constructor(
     minValue,
@@ -63,66 +66,94 @@ class NormalDistributionAnalyzer {
 
   // Generowanie 100 liczb z rozkładu normalnego i transformacja do zakresu od 1 do 10
 
-  _generateNumbers() {
-    for (let i = 0; i < this.#iterations; i++) {
-      const arrays = [];
+  _initializeFirstArray() {
+    const array = [];
 
-      for (let i = 0; i < this.#numberOfArrays; i++) {
-        arrays.push([]);
-      }
-
-      for (let i = 0; i < this.#arraySize; i++) {
-        let value = this._generateRandomNormalDistributionInRange();
-        arrays[0].push(value);
-      }
-
-      for (let i = 1; i < this.#numberOfArrays; i++) {
-        arrays[i] = [...arrays[0]];
-      }
-
-      for (let arr of arrays) {
-        const arrayShuffled = arr.sort(() => Math.random() - 0.5);
-        arrays[arrays.indexOf(arr)] = [...arrayShuffled];
-      }
-      console.log(arrays);
-
-      const sum0 = arrays[0]
-        .slice(0, this.#arrPartSingle)
-        .reduce((a, b) => a + b, 0);
-
-      this.#sumSingle.push(sum0);
-
-      let sum1_5 = 0;
-
-      for (let i = 1; i < arrays.length; i++) {
-        sum1_5 += arrays[i]
-          .slice(0, this.#arrPartMultiple)
-          .reduce((a, b) => a + b, 0);
-      }
-      this.#sumMultiple.push(sum1_5);
+    for (let i = 0; i < this.#arraySize; i++) {
+      let value = this._generateRandomNormalDistributionInRange();
+      array.push(value);
     }
+
+    return array;
+  }
+
+  _arrayInitializer() {
+    const arrays = [[...this._initializeFirstArray()]];
+
+    for (let i = 1; i < this.#numberOfArrays; i++) {
+      arrays.push([]);
+      arrays[i] = [...arrays[0]];
+    }
+    return arrays;
+  }
+
+  _shuffleArray(arrays) {
+    for (let arr of arrays) {
+      const arrayShuffled = arr.sort(() => Math.random() - 0.5);
+      arrays[arrays.indexOf(arr)] = [...arrayShuffled];
+    }
+    return arrays;
+  }
+
+  _sumSingleArray(arr) {
+    return arr[0].slice(0, this.#arrPartSingle).reduce((a, b) => a + b, 0);
+  }
+
+  _sumMultipleArray(arr) {
+    let sum = 0;
+    for (let i = 1; i < arr.length; i++) {
+      sum += arr[i].slice(0, this.#arrPartMultiple).reduce((a, b) => a + b, 0);
+    }
+
+    return sum;
+  }
+
+  _avgArrays(arr) {
+    return arr.reduce((a, b) => a + b, 0) / this.#iterations;
+  }
+
+  _generateNumbers() {
+    if (this.#arrPartSingle > this.#arraySize) {
+      console.log("Wrong array part");
+      return;
+    }
+
+    for (let i = 0; i < this.#iterations; i++) {
+      this.#arrays = this._shuffleArray(this._arrayInitializer());
+
+      this.#sumSingle.push(this._sumSingleArray(this.#arrays));
+
+      this.#sumMultiple.push(this._sumMultipleArray(this.#arrays));
+    }
+    this.#avgSingle = this._avgArrays(this.#sumSingle);
+    this.#avgMultiple = this._avgArrays(this.#sumMultiple);
+
+    this._printResults();
+  }
+
+  // Wyświetlenie wyników w konsoli
+  _printResults() {
+    console.log(this.#arrays);
+    console.log(`Average single: ${this.#avgSingle}`);
+    console.log(`Average multiple: ${this.#avgMultiple}`);
+    console.log(
+      `Difference beetwen average: ${Math.abs(
+        this.#avgMultiple - this.#avgSingle
+      ).toFixed(5)}`
+    );
+    console.log(`Min number: ${this.#minValue}`);
+    console.log(`Max number: ${this.#maxValue}`);
+    console.log(`Number of iterations: ${this.#iterations}`);
+    console.log(`Array size: ${this.#arraySize}`);
+    console.log(
+      `Number of arrays: ${this.#numberOfArrays} = (${
+        this.#numberOfArrays - 1
+      } Multiple + 1 Single)`
+    );
+    console.log(`Single array part: ${this.#arrPartSingle}`);
+    console.log(`Multiple array part: ${this.#arrPartMultiple}`);
   }
 }
 
-const ex1 = new NormalDistributionAnalyzer(1, 10, 5, 5, 7, 50);
+const ex1 = new NormalDistributionAnalyzer(1, 10, 100000, 10, 5, 5);
 ex1._generateNumbers();
-
-// Wyświetlenie wyników w konsoli
-/*
-const sumSingleAverage =
-sumSingle.reduce((a, b) => a + b, 0) / sumSingle.length;
-const sumMultipleAverage =
-sumMultiple.reduce((a, b) => a + b, 0) / sumMultiple.length;
-
-console.log("Srednia suma 50 liczb z 1. zbioru: " + sumSingleAverage);
-console.log(
-  "Srednia suma 10 liczb z 5. zbiorow tablicy: " + sumMultipleAverage
-  );
-  
-  console.log(
-    `Roznica pomiedzy srednimi: ${Math.abs(
-      sumSingleAverage - sumMultipleAverage,
-      6
-      ).toFixed(5)} przy ${sumSingle.length}. prob.`
-      );
-      */
